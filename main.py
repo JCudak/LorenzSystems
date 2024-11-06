9a685b1b1d1f4aefd3c033e6c852afa055bdc527
Bartłomiej Wiśniewski <b.wisniewski@avsystem.com>
2024-11-06 19:03:40 +0100

    add Chen, Lu and unified systems

--- main.py
+++ main.py
@@ -38,6 +38,27 @@ def yang_system(t, state, alpha, beta, gamma):
     dz_dt = x * y - beta * z
     return [dx_dt, dy_dt, dz_dt]
 
+def chen_system(t, state, alpha, beta, gamma):
+    x, y, z = state
+    dx_dt = alpha*(y - x)
+    dy_dt = (gamma - alpha)*x - x*z + gamma*y
+    dz_dt = x*y - beta*z
+    return [dx_dt, dy_dt, dz_dt]
+
+def lu_system(t, state, alpha, beta, gamma):
+    x, y, z = state
+    dx_dt = alpha*(y - x)
+    dy_dt = -x*z + gamma*y
+    dz_dt = x*y - beta*z
+    return [dx_dt, dy_dt, dz_dt]
+
+def unified_system(t, state, alpha, beta, gamma):
+    x, y, z = state
+    dx_dt = (25*alpha + 10)*(y - x)
+    dy_dt = (28 - 35*alpha)*x - x*z + (29*alpha - 1)*y
+    dz_dt = x*y - (alpha + 8)/3*z
+    return [dx_dt, dy_dt, dz_dt]
+
 
 def create_problem(num_vars, names, bounds):
     return {
@@ -89,6 +110,24 @@ def main():
             "system_func": yang_system,
             "initial_state": [1.5, 1.5, 1.5],
             "problem": create_problem(3, ['alpha', 'beta', 'gamma'], [[0.5, 20.0], [0.5, 5.0], [5.0, 30.0]])
+        },
+        {
+            "name": "Chen",
+            "system_func": chen_system,
+            "initial_state": [1.5, 1.5, 1.5],
+            "problem": create_problem(3, ['alpha', 'beta', 'gamma'], [[0.5, 20.0], [0.5, 5.0], [10.0, 35.0]])
+        },
+        {
+            "name": "Lu",
+            "system_func": lu_system,
+            "initial_state": [1.5, 1.5, 1.5],
+            "problem": create_problem(3, ['alpha', 'beta', 'gamma'], [[20.0, 40.0], [0.5, 5.0], [5.0, 30.0]])
+        },
+        {
+            "name": "Unified",
+            "system_func": unified_system,
+            "initial_state": [1.5, 1.5, 1.5],
+            "problem": create_problem(3, ['alpha', 'beta', 'gamma'], [[-2.0, 2.0], [0, 0], [0, 0]])
         }
     ]
 
