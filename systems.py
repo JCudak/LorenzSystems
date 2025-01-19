15ff187c722ae9c31cba7ea086eda2621c579b60
Bartłomiej Wiśniewski <b.wisniewski@avsystem.com>
2025-01-19 20:25:49 +0100

    implement supermodeling for lorenz system

--- systems.py
+++ systems.py
@@ -22,6 +22,28 @@ def disturbed_lorenz_system(t, state, sigma, beta, rho, eps, delta, ni):
     return [dxv_dt, dyv_dt, dzv_dt, dxh_dt, dyh_dt, dzh_dt]
 
 
+
+def lorenz_supermodel_creator(params1, params2, params3):
+    sigma1, beta1, rho1 = params1
+    sigma2, beta2, rho2 = params2
+    sigma3, beta3, rho3 = params3
+
+    def inner(t, state, c12, c13, c23, c21, c31, c32):
+        x1, y1, z1, x2, y2, z2, x3, y3, z3 = state
+
+        dx1dt, dy1dt, dz1dt = lorenz_system(t, [x1, y1, z1], sigma1, beta1, rho1)
+        dx2dt, dy2dt, dz2dt = lorenz_system(t, [x2, y2, z2], sigma2, beta2, rho2)
+        dx3dt, dy3dt, dz3dt = lorenz_system(t, [x3, y3, z3], sigma3, beta3, rho3)
+
+        dx1dt += c12*(x2-x1) + c13*(x3-x1)
+        dx2dt += c21*(x1-x2) + c23*(x3-x2)
+        dx3dt += c31*(x1-x3) + c32*(x2-x3)
+
+        return [dx1dt, dy1dt, dz1dt, dx2dt, dy2dt, dz2dt, dx3dt, dy3dt, dz3dt]
+    
+    return inner
+
+
 def yang_system(t, state, alpha, beta, gamma):
     x, y, z = state
     dx_dt = alpha * (y - x)
